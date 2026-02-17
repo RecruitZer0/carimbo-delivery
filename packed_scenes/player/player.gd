f53517c3b50c2b7e2ba103aae19d47ebace6b340
Recruit_Zer0 <59139794+RecruitZer0@users.noreply.github.com>
2026-02-17 09:45:40 -0300

    Changed AnimatedSprite2D to Sprite2D

--- packed_scenes/player/player.gd
+++ packed_scenes/player/player.gd
@@ -1,6 +1,6 @@
 class_name Player extends CharacterBody2D
 
-@onready var sprite: AnimatedSprite2D = $Sprite
+@onready var sprite: Sprite2D = $Sprite
 @onready var collision: CollisionShape2D = $Collision
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
@@ -9,6 +9,7 @@ class_name Player extends CharacterBody2D
 
 var should_spend_ink: bool = true
 var should_create_stamp: bool = true
+var ink_cooldown: float = 0.0
 
 @export var ink_amount: float = 100.0 :
 	set(value):
@@ -25,6 +26,7 @@ const STAMP_BASE_COST = 10.0
 
 
 func _physics_process(delta: float) -> void:
+	ink_cooldown = move_toward(ink_cooldown, 0, delta)
 	velocity = input.dir_input.normalized() * MOVE_SPEED * speed_multiplier
 	var collided := move_and_slide()
 	_post_move(delta, collided)
@@ -39,8 +41,9 @@ func _post_move(_delta: float, _collided: bool) -> void:
 		if should_create_stamp and ink_amount > 0:
 			create_stamp()
 			ink_amount -= stamp_cost()
-		elif sprite.frame == 1:
+		elif sprite.frame == 1 and ink_cooldown == 0:
 			ink_amount -= stamp_cost()
+			ink_cooldown = 0.4
 
 
 func create_stamp() -> Stamp:
